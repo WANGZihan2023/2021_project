3bddbd9deeecd14c1aae96aff04b8866fb6c59ad
BaiHeLuo <144666899+BaiHeLuo@users.noreply.github.com>
2024-11-19 17:38:20 +0800

    Update CVFS.java

--- CVFS.java
+++ CVFS.java
@@ -1,7 +1,24 @@
 package hk.edu.polyu.comp.comp2021.cvfs.model;
 
 public class CVFS {
-
+    private Disk disk;
+    private Directory workingDir;
+    private Criteria criteria;
     public CVFS(){}
-
+    public void setDisk(Disk disk){
+        this.disk=disk;
+    }
+    public Disk getDisk(){
+        return disk;
+    }
+    public void setDir(Directory dir){
+        workingDir=dir;
+    }
+    public Directory getDir(){
+        return workingDir;
+    }
+    public void setCriteria(Criteria criteria){this.criteria=criteria;}
+    public Criteria getCriteria(){
+        return criteria;
+    }
 }
